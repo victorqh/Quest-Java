8c30b44f4551bb5dbc2c2939b217c7f34036402b
victorqh <qhvictor@gmail.com>
2023-12-28 17:11:03 -0500

    t1

--- t1/src/Main.java
+++ t1/src/Main.java
@@ -1,15 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// 7b {
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main (String[]args){
+        int numIni = 0;
+        int numFin = 50;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (numIni < numFin){
+            numIni ++;
+            if (numIni % 2 == 0){
+                System.out.println(numIni);
+            }
         }
     }
+
 }
